17ae8a13bcd57913bf1d61a39334b72bc7a77972
Joshua O'Donnell <josh17odonnell@gmail.com>
2020-11-24 20:10:52 +0000

    Updated to accomodate review

--- TractionControlMain.py
+++ TractionControlMain.py
@@ -7,11 +7,11 @@ wheel_speed_rpm = 180
 vehicle_speed = 2 #meter/second
 torque_motor = 50 #newton/meters
  
-"If wheel speed = revolutions per revolutions per minute"
+#If wheel speed = revolutions per revolutions per minute
 c = 2 * np.pi * wheel_radius #Circumfrence of Wheel in meters
 wheel_speed_ms = (wheel_speed_rpm * c) / 60 #Gives wheel speed in m/s 
 print("wheel speed = ", wheel_speed_ms)
-"If wheel speed = m/s start here"
+
 
 def is_slipping(wheel_speed_ms, vehicle_speed):
     if vehicle_speed < wheel_speed_ms:
@@ -21,8 +21,9 @@ def is_slipping(wheel_speed_ms, vehicle_speed):
     
 is_slipping(wheel_speed_ms, vehicle_speed)
 
-if is_slipping is True and wheel_speed_ms > 0:
+if is_slipping and wheel_speed_ms > 0:
     torque_motor_new = torque_motor
     torque_motor_new -= torque_motor * (vehicle_speed / wheel_speed_ms)
+    torque_motor = torque_motor_new
     print("new motor torque =", torque_motor_new)
     
